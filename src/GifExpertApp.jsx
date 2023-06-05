69c50ecfecbc79479367721b4edc4d4cc2a47471
Everth Paredes <epe0413@gmail.com>
2023-06-04 21:25:25 -0600

    Validar que sean únicos los nombres

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,6 +6,8 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]); // Si dejamos un valor vacio este tendra un valor inicial de undefined
 
     const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory) ) return;
         setCategories([ newCategory, ...categories ]);
         // setCategories( cat => [ ...cat, 'Valorant']);
     }
