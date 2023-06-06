55e5909c839198852324a5d0f614b392d6b82b10
Everth Paredes <epe0413@gmail.com>
2023-06-06 15:51:44 -0600

    Pruebas del componente - GifItem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,13 +1,29 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas a GifItem', () => {
     const title = "Titulo de imagen"
-    const url = "test de url"
+    const url = "http://gif.test/test"
 
     test('probar snapshot', () => {
         const {container} = render(< GifItem title= {title} url={url}/>)
 
         expect( container ).toMatchSnapshot();
+    })
+
+    test('debe de mostrar la imagen con el URL y el Alt indicado', () => {
+        render(< GifItem title= {title} url={url}/>)
+
+        // expect( screen.getByRole('img').src ).toBe( url );
+        // expect( screen.getByRole('img').alt ).toBe( title );
+
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    })
+
+    test('debe mostrar el titulo en el componente', () => {
+        render(< GifItem title= {title} url={url}/>)
+        expect( screen.getByText( title )).toBeTruthy();
     })
 })
